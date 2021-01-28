7139c5e91b56630366d6c90eab6e0c28af34daa3
Kapil Bansal <kapilbansal.gbpecdelhi@gmail.com>
2021-01-29 01:28:54 +0530

    tests: Add tests to Task APIs
    Added tests for mentorship relation does not exist in Complete Task and Delete Task Tests
    
    Resolves #987

--- tests/tasks/test_dao_complete_task.py
+++ tests/tasks/test_dao_complete_task.py
@@ -2,6 +2,7 @@ import unittest
 
 from app import messages
 from app.api.dao.task import TaskDAO
+from http import HTTPStatus
 from tests.tasks.tasks_base_setup import TasksBaseTestCase
 
 
@@ -9,7 +10,7 @@ class TestCompleteTasksDao(TasksBaseTestCase):
     def test_achieve_not_achieved_task(self):
         self.assertFalse(self.tasks_list_1.find_task_by_id(1).get("is_done"))
 
-        expected_response = messages.TASK_WAS_ACHIEVED_SUCCESSFULLY, 200
+        expected_response = messages.TASK_WAS_ACHIEVED_SUCCESSFULLY, HTTPStatus.OK
         actual_response = TaskDAO.complete_task(
             self.first_user.id, self.mentorship_relation_w_second_user.id, 1
         )
@@ -36,6 +37,19 @@ class TestCompleteTasksDao(TasksBaseTestCase):
         )
 
         self.assertEqual(expected_response, actual_response)
+
+    def test_achieve_task_not_in_relation(self):
+        task_id = 1
+
+        self.assertFalse(self.tasks_list_2.find_task_by_id(task_id).get("is_done"))
+        expected_response = (
+            messages.MENTORSHIP_RELATION_DOES_NOT_EXIST,
+            HTTPStatus.NOT_FOUND,
+        )
+
+        actual_response = TaskDAO.complete_task(self.first_user.id, 123123, task_id)
+        self.assertEqual(expected_response, actual_response)
+        self.assertFalse(self.tasks_list_2.find_task_by_id(task_id).get("is_done"))
 
 
 if __name__ == "__main__":
--- tests/tasks/test_dao_delete_task.py
+++ tests/tasks/test_dao_delete_task.py
@@ -2,13 +2,14 @@ import unittest
 
 from app import messages
 from app.api.dao.task import TaskDAO
+from http import HTTPStatus
 from tests.tasks.tasks_base_setup import TasksBaseTestCase
 
 
 class TestDeleteTasksDao(TasksBaseTestCase):
     def test_delete_existent_task(self):
 
-        expected_response = messages.TASK_WAS_DELETED_SUCCESSFULLY, 200
+        expected_response = messages.TASK_WAS_DELETED_SUCCESSFULLY, HTTPStatus.OK
         first_task_id = 1
 
         not_deleted_yet_task = self.tasks_list_1.find_task_by_id(task_id=first_task_id)
@@ -22,9 +23,23 @@ class TestDeleteTasksDao(TasksBaseTestCase):
         deleted_task = self.tasks_list_1.find_task_by_id(task_id=first_task_id)
         self.assertIsNone(deleted_task)
 
+    def test_delete_task_not_in_relation(self):
+        expected_response = (
+            messages.MENTORSHIP_RELATION_DOES_NOT_EXIST,
+            HTTPStatus.NOT_FOUND,
+        )
+        second_task_id = 2
+
+        second_task = self.tasks_list_1.find_task_by_id(task_id=second_task_id)
+        self.assertIsNotNone(second_task)
+
+        actual_response = TaskDAO.delete_task(self.first_user.id, 4, second_task_id)
+
+        self.assertEqual(expected_response, actual_response)
+
     def test_delete_non_existent_task(self):
 
-        expected_response = messages.TASK_DOES_NOT_EXIST, 404
+        expected_response = messages.TASK_DOES_NOT_EXIST, HTTPStatus.NOT_FOUND
         actual_response = TaskDAO.delete_task(
             self.first_user.id, self.mentorship_relation_w_second_user.id, 123123
         )
